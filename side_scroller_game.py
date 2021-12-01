7d2df58608b74fc5d17aeb7e3d4b7050cfb5204e
michaeldolan123 <mqdolan24@students.bmchs.org>
2021-12-01 09:21:15 -0600

    12345

--- side_scroller_game.py
+++ side_scroller_game.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from pygame import display
 from pygame.locals import *
 import random, time
  
@@ -13,7 +14,7 @@ GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-SCREEN_WIDTH = 2000
+SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 500
 SPEED = 5
 SCORE = 0
@@ -33,39 +34,36 @@ pygame.display.set_caption("Game")
 class Enemy(pygame.sprite.Sprite):
       def __init__(self):
         super().__init__() 
-        self.image = pygame.image.load("oil-spill.png")
+        self.image = pygame.image.load("side_scroller.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(40,SCREEN_WIDTH-40), 0)
+        self.rect.center = (SCREEN_WIDTH, random.randint(0,SCREEN_HEIGHT))
 
       def move(self):
         global SCORE
-        self.rect.move_ip(0,SPEED)
-        if (self.rect.bottom > SCREEN_WIDTH):
+        self.rect.move_ip(-SPEED, 0)
+        if (self.rect.left <= 0):
             SCORE += 1
-            self.rect.top = 0
-            self.rect.center = (player.rect.x, 0)
+            self.rect.right = SCREEN_HEIGHT / 2
+            self.rect.center = (SCREEN_WIDTH, random.randint(0,SCREEN_HEIGHT))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
         self.image = pygame.image.load("Porsche.png")
+        self.image = pygame.transform.rotate(self.image, 180)
         self.rect = self.image.get_rect()
 
        
     def move(self):
         pressed_keys = pygame.key.get_pressed()
         
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-20, 0)
-        
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(20, 0)
 
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -20)
+
+        if pressed_keys[K_UP] and self.rect.top >= 0:
+            self.rect.move_ip(0, -10)
                 
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 20)
+        if pressed_keys[K_DOWN] and self.rect.bottom <= SCREEN_HEIGHT:
+            self.rect.move_ip(0, 10)
 
 
         
@@ -82,9 +80,10 @@ all_sprites.add(E1)
 INC_SPEED = pygame.USEREVENT + 1
 pygame.time.set_timer(INC_SPEED, 1000)
 
-while True:
-    DISPLAYSURF.blit(pygame.image.load("road.png"), (0,0))
+DISPLAYSURF.blit(pygame.image.load("road.png"), (0, 0))
 
+while True:
+    
     for event in pygame.event.get():
         if event.type == INC_SPEED:
               SPEED += 1     
@@ -92,7 +91,7 @@ while True:
             pygame.quit()
             sys.exit()
 
-    DISPLAYSURF.blit(background, (0,0))
+    # DISPLAYSURF.blit(background, (0,0))
     scores = font_small.render(str(SCORE), True, BLACK)
     DISPLAYSURF.blit(scores, (10,10))
 
@@ -112,7 +111,14 @@ while True:
                 entity.kill() 
           time.sleep(2)
           pygame.quit()
-          sys.exit()        
+          sys.exit()   
+          
+    pygame.display.update()      
+    if rect_of_background.right > SCREEN_WIDTH:
+        rect_of_background.center = (SCREEN_WIDTH/2 - 3, SCREEN_HEIGHT/2)
+        DISPLAYSURF.blit(background, rect_of_background)
+    # if rect_of_background.right <= SCREEN_WIDTH:
+    #     rect_of_background.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
         
     pygame.display.update()
     FramePerSec.tick(FPS)
